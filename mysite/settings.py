e10011a9b0badee7c54ea6302812929d1888deca
matizaidan <leandromatiaszaidan@gmail.com>
2024-04-02 00:33:31 -0300

    Agregando Banner como static

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,5 +123,5 @@ STATICFILES_DIRS = [
 
 # Media files
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
