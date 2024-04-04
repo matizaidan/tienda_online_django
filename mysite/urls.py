f42d83aa7ef408c9ff5b726bde13a79cf8f13e5c
matizaidan <leandromatiaszaidan@gmail.com>
2024-04-04 12:00:15 -0300

    Modificaciones: Correccion de urls, coreccion de base, arreglado de stylo y funcionalidad responssive a las plantillas

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 from products import views as products_views
 
 urlpatterns = [
-    path('', products_views.home, name='home'),
+    path('', include('products.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('products/', include('products.urls')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
